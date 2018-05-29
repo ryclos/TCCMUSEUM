f804ab638d884c615119368266744a52846cb996
ryclos <loogle@hotmail.com>
2018-05-29 09:43:14 -1000

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from "../pages/welcome/welcome";
+import { ScanPage } from "../pages/scan/scan";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     AboutPage,
     HomePage,
     WelcomePage,
-    TabsPage
+    TabsPage,
+    ScanPage
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     AboutPage,
     HomePage,
     WelcomePage,
-    TabsPage
+    TabsPage,
+    ScanPage
   ],
   providers: [
     StatusBar,
