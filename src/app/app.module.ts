4260f18ddae73dd6d764a359f63f47e8a985aae6
Vai987 <vmonaco.tcc@gmail.com>
2018-05-29 15:34:17 -1000

    sqlite add2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { SQLite } from '@ionic-native/sqlite';
 
+//pages
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { WelcomePage } from "../pages/welcome/welcome";
 import { ScanPage } from "../pages/scan/scan";
+import { SqlitePage } from "../pages/sqlite/sqlite";
 
+//fonctionnalitees
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
@@ -21,6 +25,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     HomePage,
     WelcomePage,
     TabsPage,
+    SqlitePage,
     ScanPage
   ],
   imports: [
@@ -34,12 +39,14 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     HomePage,
     WelcomePage,
     TabsPage,
+    SqlitePage,
     ScanPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    SQLite,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
